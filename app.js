85a9d0284b9fc8500b5a9f9ffb3b061e8f4448da
BhuvanVarma <bhuvan.varma123@gmail.com>
2021-10-26 23:12:01 +0530

    added files

---
+++ app.js
@@ -0,0 +1,7 @@
+const { add, sub, mul, div, message} = require('./utils')
+
+add()
+sub()
+mul()
+div()
+console.log(message);
---
+++ utils.js
@@ -0,0 +1,20 @@
+const add = () => {
+    console.log('I add stuff');
+}
+
+const sub = () => {
+    console.log('I substract stuff');
+}
+
+const mul = () => {
+    console.log('I multiply stuff');
+}
+
+const div = () => {
+    console.log('I divide stuff');
+}
+module.exports.add = add
+module.exports.sub = sub
+module.exports.mul = mul
+module.exports.div = div
+module.exports.message = "MESSAGE"
